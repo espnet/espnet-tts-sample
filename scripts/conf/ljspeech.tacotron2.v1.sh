1d6d307ac90db4e82bff1e41eb3c9fec8274dcbb
potato-inoue <k_inoue@a.cs.okayama-u.ac.jp>
2019-09-14 08:46:16 +0900

    update conf

--- scripts/conf/ljspeech.tacotron2.v1.sh
+++ scripts/conf/ljspeech.tacotron2.v1.sh
@@ -6,5 +6,5 @@
 corpus="ljspeech"
 model="tacotron2.v1"
 github_id="kan-bayashi" 
-vocoder_num=2 # 1:only GL, 2:GL&WNV
-demo_num=1 # 1:only wav, 2:wav&att, 3:wav&att&sp
+vocoder_num=1 # 1:only GL, 2:GL&WNV
+demo_num=2 # 1:only wav, 2:wav&(att_ws,probs), 3:wav&(att_ws,probs)&sp
